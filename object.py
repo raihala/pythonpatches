641b5a4991a83cf4c351e1daa1143ee13ad48f0a
Juno Raihala <jcraihala@gmail.com>
2016-06-01 19:15:46 -0500

    Create object.py

---
+++ object.py
@@ -0,0 +1,34 @@
+import random
+import time
+
+class Object(object):
+	"""
+	An improvement over the default object class.
+	"""
+	__doc__ = "None"
+	
+	def __call__(self):
+		return None
+	
+	def __getattr__(self, name):
+		time.sleep(random.random() * 10)
+		raise OSError("Python encountered a problem. "
+					  "You must reboot your computer!")
+	
+	def __setattr__(self, name, value):
+		pass
+	
+	def __repr__(self):
+		return "None"
+
+	def __dir__(self):
+		return []
+	
+	def __cmp__(self, other):
+		return random.random() > 0.5
+	
+	def __nonzero__(self):
+		return random.random() > 0.5
+	
+	def __del__(self):
+		time.sleep(random.random() * 5)
